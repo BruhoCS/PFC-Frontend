a7f26c93fa2e72e0ee15867862f2188c3865145a
Bruno Couceiro <sande.couceiro@gmail.com>
2025-12-07 15:59:37 +0100

    Opcion de admin en cabecera + solución problema en agregar un nuevo entreno

--- src/app/vista-general/cabecera/cabecera.component.ts
+++ src/app/vista-general/cabecera/cabecera.component.ts
@@ -1,25 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { Usuario } from '../modelo/usuario';
+import { UsuariosService } from '../../servizos/usuarios.service';
 
 
 @Component({
   selector: 'app-cabecera',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './cabecera.component.html',
   styleUrl: './cabecera.component.css'
 })
-export class CabeceraComponent {
+export class CabeceraComponent implements OnInit {
 
-  constructor(private router:Router){
-    
+
+  usuarioActual: any = null;
+
+  constructor(private router: Router, private servicioUsuario: UsuariosService) { }
+
+  ngOnInit() {
+    // Suscribirse a los cambios del usuario en tiempo real
+    this.servicioUsuario.usuarioActual$.subscribe(user => {
+      this.usuarioActual = user;
+    });
+
+    // Por si había sesión guardada
+    const storedUser = sessionStorage.getItem("usuarioActual");
+    if (storedUser) {
+      this.usuarioActual = JSON.parse(storedUser);
+      this.servicioUsuario.usuarioActual$.next(this.usuarioActual);
+    }
   }
-
-  cerrarSesion(){
+  cerrarSesion() {
     sessionStorage.clear();
     //Reenviamos a la página de inicio
-    this.router.navigate(['']);
+    this.router.navigate(['/login']);
   }
 
 }
+
